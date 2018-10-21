604ca65d5c0ed5d8f01ccb8f26656ddae7686d30
foolmarks <16252167+foolmarks@users.noreply.github.com>
2018-10-21 13:54:58 +0200

    Update cifar10.py

--- cifar10.py
+++ cifar10.py
@@ -1,12 +1,8 @@
 '''
-Cifar-10 classification
-Original dataset and info: https://www.cs.toronto.edu/~kriz/cifar.html for more information
-See: https://www.bonaccorso.eu/2016/08/06/cifar-10-image-classification-with-keras-convnet/ for further information
+CIFAR10 example using KEras & TensorFlow
 '''
 
 import numpy as np
-#import matplotlib.pyplot as plt
-
 
 from keras.callbacks import EarlyStopping
 from keras.datasets import cifar10
@@ -18,70 +14,56 @@ from keras.layers.pooling import MaxPooling2D
 from keras.utils import to_categorical
 
 
-
-
-# For reproducibility
-np.random.seed(1000)
-
-if __name__ == '__main__':
-
 ##############################################
 # Preparation of input dataset
 ##############################################
 # CIFAR10 datset has 60k images. Trainge set is 50k, test set is 10k.
 # Each image is 32x32 pixels RGB
-
-    (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+(X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
 
 
 ##############################################
 # Keras Sequential model
 ##############################################
+# Create the model
+model = Sequential()
 
-    # Create the model
-    model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
 
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
-
-    # print a summary of the model
-    print(model.summary())
+# print a summary of the model
+print(model.summary())
 
 ##############################################
 # Compile model
 ##############################################
-    # Compile the model
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(lr=0.0001, decay=1e-6),
-                  metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
 
 ##############################################
 # Train model with training set
 ##############################################
-    model.fit(X_train / 255.0, to_categorical(Y_train),
-              batch_size=128,
-              shuffle=True,
-              epochs=5,
-              validation_data=(X_test / 255.0, to_categorical(Y_test)),
-              callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
+model.fit(X_train / 255.0, to_categorical(Y_train),
+          batch_size=128,
+          shuffle=True,
+          epochs=5,
+          validation_data=(X_test / 255.0, to_categorical(Y_test)),
+          callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
 
 ##############################################
 # Evaluate model accuracy with test set
 ##############################################
-    scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
+scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
 
-    print('Loss: %.3f' % scores[0])
-    print('Accuracy: %.3f' % scores[1])
+print('Loss: %.3f' % scores[0])
+print('Accuracy: %.3f' % scores[1])
